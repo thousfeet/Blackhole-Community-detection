5b8c98e7e8f9727b453f3a6add4a086bbcb56b48
thousfeet <848804259@qq.com>
2019-12-19 21:23:12 +0800

    Init DBscan

--- BlackHoleCD/BlackHoleCD/DBScan.cpp
+++ BlackHoleCD/BlackHoleCD/DBScan.cpp
@@ -1,11 +1,11 @@
 #include "DBScan.h"
-
-NodeCIDSet DBScan::dbscan(const NodePosSet& nodePoses, const float eps, const int minPts)
-{
-
-    return NodeCIDSet();
-}
 #include <iostream>
 
-	// Just Place here, you can delete it;
-	DataUtils::writeNodePoses(".\\", "test", nodePoses);
+
+NodeCIDSet DBScan::dbscan(NodePosSet& nodePoses, const float eps, const int minPts)
+{
+    DataUtils::writeNodePoses(".\\", "test", nodePoses);
+    return NodeCIDSet();
+}
+
+
--- BlackHoleCD/BlackHoleCD/DBScan.h
+++ BlackHoleCD/BlackHoleCD/DBScan.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#include "DataUtils.h"
+#include "GraphDrawing.h"
 
 class DBScan
 {
 public:
 	/*
-	dbscan����˵��
+	dbscan函数说明
 
-	���ã�DBScan����
-	���룺nodePoses��vector<pair<�ڵ㣬Pos>>�� 
-	�����nodeCIDs��vector<pair<�ڵ�, ������Cluster>>
+	作用：DBScan聚类
+	输入：nodePoses：vector<pair<节点，Pos>>， 
+	输出：nodeCIDs：vector<pair<节点, 所属的Cluster>>
 	*/
-	static NodeCIDSet dbscan(const NodePosSet& nodePoses, const float eps, const int minPts);
+	static NodeCIDSet dbscan(NodePosSet& nodePoses, const float eps, const int minPts);
 };
 
