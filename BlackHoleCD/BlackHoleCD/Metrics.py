e64dbeada58f3c352adc95ae4e3fa72414777d1e
thousfeet <848804259@qq.com>
2019-12-21 21:42:07 +0800

    Metrics fix bug

--- BlackHoleCD/BlackHoleCD/Metrics.py
+++ BlackHoleCD/BlackHoleCD/Metrics.py
@@ -19,9 +19,9 @@ clusterFilePath = rootPath + "football.nodeCIDs.txt"
 # internal density (M1), ms / (ns*(ns-1)/2)
 # edges inside(M2), ms
 # average degree (M3), 2*ms / ns;
-# fraction over median degree (M4), |指向社区内的边数>所有节点边数中位数 的节点|/ns
+# fraction over median degree (M4), |指向社区内的边数>社区内所有节点边数中位数 的节点|/ns
 # expansion (M5), cs/ns
-# cut ratio (M6),  cs / ns(n-ns)
+# cut ratio (M6),  cs / ns*(n-ns)
 # conductance (M7), cs / (2*ms+cs)
 # normalized cut (M8), M7 + cs / (2*(m-ms)+cs)
 # flake out degree fraction (M9), |指向社区内的边数<指向社区外的边数 的节点|/ns
@@ -29,7 +29,7 @@ clusterFilePath = rootPath + "football.nodeCIDs.txt"
 # 整张图的总边数m、节点数n
 # 一个社区的内部边数ms、节点数ns
 # cs = 社区内节点指向社区外的边数和
-# 一个节点的度数d、指向社区内社区外的度数di,do
+# 一个节点的度数d、指向社区内社区外的度数
 ########
 
 def M1(ms, ns):
@@ -42,11 +42,18 @@ def M3(ms, ns):
     return 2*ms / ns
 
 def M4(node_di, node_edge, ns):
-    mid = np.median(np.array(list(node_edge.values())))
+    edges = []
+    for node in node_di:
+        edges.append(int(node_edge[node]))
+    mid = np.median(np.array(edges))
+    # print("mid", mid)
     cnt = 0
     for node in node_di:
-        if node_di[node] > mid: cnt+=1
+        if node_di[node] > mid: cnt += 1
+    # print(node_di)
+    # print("cnt",cnt)
     return cnt/ns
+
 def M5(cs, ns):
     return cs / ns
 
@@ -62,10 +69,18 @@ def M8(cs, m, ms):
 def M9(node_di, node_edge):
     cnt = 0
     for node in node_di:
-        if node_di[node] < node_edge[node]/2: cnt+=1
+        if node_di[node] < node_edge[node]/2: cnt += 1
     return cnt/ns
 
+
 #############################################
+# node_node dict{node, {相邻node}}
+# node_CID dict{node, CID}
+# CID_node dict{CID, {nodes}}
+# node_edge dict{node, 度数}
+# node_di dict{node, 指向cluster内的边数} （对于某cluster）
+#############################################
+
 
 m = 0
 node_node = defaultdict(set)
@@ -75,14 +90,14 @@ with open(dataFilePath) as dataFile:
         if line.startswith('#'):
             continue
         m += 1
-        u, v = line.split()
+        u, v = line.split()[:2]
         node_node[u].add(v)
         node_node[v].add(u)
 
-# for ne in node_edge:
-#     print(ne, node_edge[ne])
-
 n = len(node_node)
+node_edge = dict()
+for node in node_node:
+    node_edge[node] = len(node_node[node])
 
 node_CID = dict()
 CID_node = defaultdict(set)
@@ -90,36 +105,36 @@ with open(clusterFilePath) as clusterFile:
     for line in clusterFile:
         if line.startswith('#'):
             continue
-        node, cid = line.split()
+        node, cid = line.split()[:2]
         node_CID[node] = cid
         if int(cid) >= 0:
             CID_node[cid].add(node)
-# for cluster in CID_node:
-#     print(cluster, CID_node[cluster])
 
-# node_node dict{node, {相邻node}}
-# node_CID dict{node, CID}
-# CID_node dict{CID, {nodes}}
-
-node_di = dict()  # 各节点指向cluster内的边数
-node_edge = dict()
-print("cluster", len(CID_node))
+node_di = dict()
+# print("cluster", len(CID_node))
 for cluster in CID_node:
+    edge_cnt = 0  # 社区内所有节点的度数和
     node_di.clear()
     nodes = CID_node[cluster]
     ns = len(nodes)
     for u in nodes:
         vs = node_node[u]  # u节点的相邻节点set
-        node_edge[u] = len(vs)
-        # print(vs)
+        edge_cnt += len(vs)
         for v in vs:
-            # print(v)
             if v in nodes:
                 if u not in node_di: node_di[u] = 1
                 else: node_di[u] += 1
-    edge_cnt = sum(map(int, node_edge)) # 所有节点的度数和
-    ms = sum(map(int, node_di))/2
-    cs = edge_cnt-sum(map(int,node_di))
-    print("node number in this cluster", ns)
+    ms = sum(map(int, node_di.values()))/2
+    cs = edge_cnt-sum(map(int, node_di.values()))
+    # print("node_node", node_node)
+    # print("node_di", node_di)
+
+    # tmp = 0
+    # for node in node_di:
+    #     tmp += len(node_node[node])-node_di[node]
+    # print("tmp", tmp)
+
+    print("edge_cnt", edge_cnt, "ns", ns, "ms", ms, "cs", cs)
     print(M1(ms, ns), M2(ms), M3(ms, ns), M4(node_di, node_edge, ns), M5(cs, ns),
           M6(cs, n, ns), M7(cs, ms), M8(cs, m, ms), M9(node_di, node_edge))
+    break
