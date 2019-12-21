c60029091c5638d5e45dc86540689d42980fbc19
thousfeet <848804259@qq.com>
2019-12-21 15:57:29 +0800

    modify data types

--- BlackHoleCD/BlackHoleCD/type.h
+++ BlackHoleCD/BlackHoleCD/type.h
@@ -6,21 +6,21 @@
 
 
 /********************************************
-����˵��
+类型说明
 
-Node - unsigned int: �ڵ�ID
-NodeSet - unordered_set: Node����
+Node - unsigned int: 节点ID
+NodeSet - unordered_set: Node集合
 
-Edge - long long: ��Ȩ
-EdgeSet - unordered_set: Edge����
+Edge - long long: 边权
+EdgeSet - unordered_set: Edge集合
 
-CID - int: Cluster��ID
+CID - int: Cluster的ID
 ClusterSet - unordered_map: <CID, NodeSet>
 
-Pos - vector: {<double>} �ڵ��ڿռ��е�λ��
+Pos - vector: {<double>} 节点在空间中的位置
 NodePosSet - unordered_map: <Node, Pos>
 
-NodeCID - unordered_map: <Node, CID> �ڵ�������cluster
+NodeCID - unordered_map: <Node, CID> 节点所属的cluster
 NodeNearPoints - unordered_map: <Node, NodeSet>
 ***********************************************/
 
@@ -30,6 +30,4 @@ typedef std::unordered_map<CID, NodeSet> ClusterSet;
 
 typedef std::unordered_map<Node, Pos> NodePosSet;
 typedef std::unordered_map<Node, CID> NodeCID;
-
-typedef std::unordered_map<Node, Pos> NodePosSet;
 typedef std::unordered_map<Node, NodeSet> NodeNearPoints;
